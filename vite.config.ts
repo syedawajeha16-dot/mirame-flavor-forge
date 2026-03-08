2757e0c8a4b03047e72278c221ae05499b991110
syedawajeha16-dot <syedawajeha16@gmail.com>
2026-03-08 17:20:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/mirame-flavor-fury/',
   server: {
     host: "::",
     port: 8080,
